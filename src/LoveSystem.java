f6a5c6f230986abc0c23bd7514e069853b46f019
Monsler <105060825+Monsler@users.noreply.github.com>
2025-04-20 18:56:28 +0000

    New func + major version change
    + Implemented function love.system.openURL
    + major version has changed to 0.4.7

--- src/LoveSystem.java
+++ src/LoveSystem.java
@@ -2,6 +2,8 @@ import org.luaj.vm2.LuaValue;
 import org.luaj.vm2.lib.OneArgFunction;
 import org.luaj.vm2.lib.ZeroArgFunction;
 
+import javax.microedition.io.ConnectionNotFoundException;
+
 public class LoveSystem {
     public static class getOs extends ZeroArgFunction {
         public LuaValue call() {
@@ -17,10 +19,23 @@ public class LoveSystem {
         }
     }
 
+    public static class openUrl extends OneArgFunction {
+        public LuaValue call(LuaValue luaValue) {
+            try {
+                LoveCanvas.currentMidlet.platformRequest(luaValue.checkjstring());
+            } catch (ConnectionNotFoundException e) {
+                return LuaValue.valueOf(false);
+            }
+
+            return LuaValue.valueOf(true);
+        }
+    }
+
     public static LuaValue create() {
         LuaValue systemTable = LuaValue.tableOf();
         systemTable.set("getOS", new getOs());
         systemTable.set("vibrate", new vibrate());
+        systemTable.set("openURL", new openUrl());
         return systemTable;
     }
 }
